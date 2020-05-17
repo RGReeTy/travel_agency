8f99c67c9e716f731dbcbaf37d87899cd523f915
RGReeTy <be.a.reety@gmail.com>
2020-05-17 18:01:40 +0300

    added static method for counting final price (include discount)

---
+++ src/main/java/by/epam/travel_agency/service/manager/FinalPriceMaker.java
@@ -0,0 +1,22 @@
+package by.epam.travel_agency.service.manager;
+
+import by.epam.travel_agency.bean.Request;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+public class FinalPriceMaker {
+    public static final BigDecimal ONE_HUNDRED = new BigDecimal(100);
+
+    public static Set<Request> countFinalPrice(Set<Request> set) {
+        for (Request req : set) {
+            req.setFinalCount(req.getCount().subtract(percentage(req.getCount(), req.getDiscount())));
+        }
+        return set;
+    }
+
+
+    private static BigDecimal percentage(BigDecimal base, int pct) {
+        return base.multiply(BigDecimal.valueOf(pct)).divide(ONE_HUNDRED);
+    }
+}
